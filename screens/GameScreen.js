8b996c220eecb17d92f2885c4e1733b328f0aa03
Jaime Melis <jmelis@redhat.com>
2020-11-14 08:57:11 +0100

    working styles more or less

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import { Component } from 'react';
 import ProgressBar from 'react-native-progress/Bar';
 
 import mathQuestion from '../lib/math-question';
+
+const GAME_LENGTH = 120; // in seconds
 
 function shuffleArray(_array) {
   const array = _array.slice();
@@ -14,6 +16,16 @@ function shuffleArray(_array) {
     array[j] = temp;
   }
   return array;
+}
+
+// <Button buttonStyle={{ flex: 1 }} disabled={!this.state.button1.status}
+// onPress={() => this.handleClickButton(this.state.button1)}
+// title={this.state.button1.val.toString()} />
+
+function AnsButton({ title, disabled, handler}) {
+  return <TouchableOpacity style={styles.touchable} disabled={disabled} onPress={() => handler()}>
+    <Text style={disabled ? styles.buttondisabled : styles.button}>{title}</Text>
+  </TouchableOpacity>
 }
 
 class GameScreen extends Component {
@@ -37,7 +49,7 @@ class GameScreen extends Component {
   componentDidMount() {
     const startTime = new Date();
     const interval = setInterval(() => {
-      const percent = 1 - (new Date() - startTime) / 5000;
+      const percent = 1 - (new Date() - startTime) / (GAME_LENGTH * 1000);
       this.setState({ percent });
 
       if (percent < 0) {
@@ -79,17 +91,55 @@ class GameScreen extends Component {
   }
 
   render() {
-    return <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Level: {this.props.route.params.level}</Text>
-      <Text>Score: {this.state.score}</Text>
-      <Text>{this.state.question}</Text>
-      <ProgressBar progress={this.state.percent} width={200} borderRadius={0} height={20} animationType={"timing"} />
-      <Button disabled={!this.state.button1.status} onPress={() => this.handleClickButton(this.state.button1)} title={this.state.button1.val.toString()} />
-      <Button disabled={!this.state.button2.status} onPress={() => this.handleClickButton(this.state.button2)} title={this.state.button2.val.toString()} />
-      <Button disabled={!this.state.button3.status} onPress={() => this.handleClickButton(this.state.button3)} title={this.state.button3.val.toString()} />
-      <Button disabled={!this.state.button4.status} onPress={() => this.handleClickButton(this.state.button4)} title={this.state.button4.val.toString()} />
+    return <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <Text style={{ flex: 1 }}>Level: {this.props.route.params.level}</Text>
+        <Text style={{ flex: 1, textAlign: 'right' }}>Score: {this.state.score}</Text>
+      </View>
+      <Text style={{ flex: 1, fontSize: 200, textAlign: 'center', fontSize: 0.30 * Dimensions.get('window').width }}>{this.state.question}</Text>
+      <ProgressBar style={styles.progressbar} width={null} progress={this.state.percent} borderRadius={0} height={30} />
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <AnsButton title={this.state.button1.val.toString()} disabled={!this.state.button1.status} handler={() => this.handleClickButton(this.state.button1)} />
+        <AnsButton title={this.state.button2.val.toString()} disabled={!this.state.button2.status} handler={() => this.handleClickButton(this.state.button2)} />
+      </View>
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <AnsButton title={this.state.button3.val.toString()} disabled={!this.state.button3.status} handler={() => this.handleClickButton(this.state.button3)} />
+        <AnsButton title={this.state.button4.val.toString()} disabled={!this.state.button4.status} handler={() => this.handleClickButton(this.state.button4)} />
+      </View>
     </View>;
   }
 }
+
+const styles = StyleSheet.create({
+  progressbar: {
+    flex: 1,
+    justifyContent: 'center',
+    color: '#4978c4',
+    borderWidth: 0,
+    padding: 10,
+  },
+  touchable: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: "#eaeaea",
+  },
+  button: {
+    backgroundColor: '#4978c4',
+    textAlign: "center",
+    alignSelf: "stretch",
+    padding: 30,
+    color: "white",
+    fontSize: 40,
+  },
+  buttondisabled: {
+    backgroundColor: '#aaaaaa',
+    textAlign: "center",
+    alignSelf: "stretch",
+    padding: 30,
+    color: "white",
+    fontSize: 40,
+  },
+});
+
 
 export default GameScreen;
