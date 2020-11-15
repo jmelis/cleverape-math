dbc01908ee6a4aad82ad2e89dd4d676e653c727c
Jaime Melis <jmelis@redhat.com>
2020-11-15 13:13:52 +0100

    show new record

--- screens/GameResults.js
+++ screens/GameResults.js
@@ -1,6 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const NewRecord = () => (
+  <Text style={styles.newrecord}>
+    NEW RECORD!
+    &nbsp;
+    <FontAwesome5 name="trophy" style={styles.newrecordCup} />
+  </Text>
+);
 
 function GameResults({ navigation, route }) {
   const [maxScore, setMaxScore] = useState(0);
@@ -32,11 +41,11 @@ function GameResults({ navigation, route }) {
 
   return (
     <View style={{ flex: 1, padding: 10 }}>
-      <View style={{ flex: 10, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{ flex: 3, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
         <Text style={{ fontSize: 90 }}>{route.params.score}</Text>
         <Image style={{ width: 100, height: 100 }} source={require('../img/star.png')} />
       </View>
-      <Text style={{ flex: 1 }}>{maxScore}</Text>
+      {route.params.score != 0 && route.params.score === maxScore && <NewRecord/>}
       <TouchableOpacity style={styles.touchable} onPress={() => navigation.push('GameScreen', { level: route.params.level })}>
         <Text style={styles.button}>Play again</Text>
       </TouchableOpacity>
@@ -48,8 +57,17 @@ function GameResults({ navigation, route }) {
 }
 
 const styles = StyleSheet.create({
-  touchable: {
+  newrecord: {
     flex: 1,
+    textAlign: 'center',
+    fontSize: 40
+  },
+  newrecordCup: {
+    fontSize: 40,
+    color: "#f5e920"
+  },
+  touchable: {
+    flex: 2,
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'stretch',
@@ -60,6 +78,7 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
     backgroundColor: '#18427c',
     color: "white",
+    fontSize: 50,
   },
 });
 
